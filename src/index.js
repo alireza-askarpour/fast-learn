fd724fe5656332282aa0f3c57bf07ff008f52dec
Alireza Askarpour <askarpourdev@gmail.com>
2023-03-07 07:25:05 -0500

    feat: add morgan middleware

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import createHttpError from 'http-errors'
 import { appListener, appErrorHandler, port } from './config/app.config.js'
 
 import allRoutes from './routes/index.js'
+import { morganMiddleware } from './middlewares/morgan.middlewares.js'
 
 // Config
 dotenv.config()
@@ -12,6 +13,7 @@ dotenv.config()
 const app = express()
 
 // Middlewares
+app.use(morganMiddleware)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/uploads', express.static('uploads'))
@@ -20,14 +22,9 @@ app.use('/uploads', express.static('uploads'))
 app.use(allRoutes)
 
 // Error Handler
-app.use(
-  ('*',
-  (req, res, next) => {
-    next(
-      createHttpError.NotFound(`Can't find ${req.originalUrl} on the server!`)
-    )
-  })
-)
+app.use('*', (req, res, next) => {
+  next(createHttpError.NotFound(`Can't find ${req.originalUrl} on the server!`))
+})
 app.use(appErrorHandler)
 
 // Listener
