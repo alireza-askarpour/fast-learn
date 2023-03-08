f868b09b483ac2f5088b0e1bea7839d4728e5d9e
Alireza Askarpour <askarpourdev@gmail.com>
2023-03-08 14:19:02 -0500

    feat: add user model

---
+++ src/models/user.models.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+import { nanoid } from 'nanoid'
+import {
+  alphabetLowerCaseLetters,
+  alphabetNumber,
+} from '../config/nanoid.config'
+
+const UserModel = new mongoose.Schema(
+  {
+    first_name: { type: String },
+    last_name: { type: String },
+    username: {
+      type: String,
+      lowercase: true,
+      default: nanoid(alphabetNumber + alphabetLowerCaseLetters, 10),
+    },
+    mobile: { type: String, required: true },
+    otp: {
+      type: Object,
+      default: {
+        code: 0,
+        expiresIn: 0,
+      },
+    },
+    role: {
+      type: String,
+      required: true,
+      default: 'student',
+      enum: ['student', 'writer', 'admin'],
+    },
+    avater: { type: String, default: '' },
+    courses: { type: [mongoose.Types.ObjectId], default: [] },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export default mongoose.model(UserModel, 'user')
