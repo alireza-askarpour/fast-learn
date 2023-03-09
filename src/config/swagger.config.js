5546154c4224645c4c30054cbf53a570280449ec
Alireza Askarpour <askarpourdev@gmail.com>
2023-03-09 10:10:28 -0500

    feat: install and config swagger ui

---
+++ src/config/swagger.config.js
@@ -0,0 +1,35 @@
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUI from 'swagger-ui-express'
+
+export const swaggerSetup = swaggerUI.setup(
+  swaggerJSDoc({
+    swaggerDefinition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'FastLearn',
+        version: '1.0.0',
+        description: 'The online web development learning website.',
+        contact: {
+          name: 'Alireza Askarpour',
+          email: 'askarpourdev@gmail.com',
+        },
+      },
+      server: [
+        {
+          url: process.env.BASE_URL,
+        },
+      ],
+      components: {
+        securitySchemes: {
+          BearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+          },
+        },
+      },
+      security: [{ BearerAuth: [] }],
+    },
+    apis: ['./src/routes/swagger/*.js'],
+  })
+)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import createHttpError from 'http-errors'
+import swaggerUI from 'swagger-ui-express'
 
 import { appListener, appErrorHandler, port } from './config/app.config.js'
 import connectDB from './config/database.config.js'
 import allRoutes from './routes/index.routes.js'
 
 import { morganMiddleware } from './middlewares/morgan.middleware.js'
+import { swaggerSetup } from './config/swagger.config.js'
 
 // Config
 dotenv.config()
@@ -19,6 +21,9 @@ app.use(morganMiddleware)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/uploads', express.static('uploads'))
+
+// Settings
+app.use('/docs', swaggerUI.serve, swaggerSetup)
 
 // Routes
 app.use(allRoutes)
