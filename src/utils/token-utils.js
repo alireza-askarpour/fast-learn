ddaa35dd0ae06e6b4cbbc4cd69db503e435c04e9
Alireza Askarpour <askarpourdev@gmail.com>
2023-03-09 07:04:57 -0500

    feat: add signe refresh token and verify refresh token utils

--- src/utils/token-utils.js
+++ src/utils/token-utils.js
@@ -19,3 +19,34 @@ export const signAccessToken = userId => {
     })
   })
 }
+
+export const signRefreshToken = userId => {
+  return new Promise(async (resolve, reject) => {
+    const user = await UserModels.findById(userId)
+
+    const payload = {
+      mobile: user.mobile,
+    }
+    const options = {
+      expiresIn: '1y',
+    }
+
+    JWT.sign(payload, process.env.REFRESH_TOKEN_SECRET_KEY, options, async (err, token) => {
+      if (err) reject(createHttpError.InternalServerError())
+      resolve(token)
+    })
+  })
+}
+
+export const verifyRefreshToken = token => {
+  return new Promise((resolve, reject) => {
+    JWT.verify(token, process.env.REFRESH_TOKEN_SECRET_KEY, async (err, payload) => {
+      if (err) reject(createError.Unauthorized('Login to your account'))
+
+      const user = await UserModels.findOne({ mobile: payload.mobile }, { password: 0, otp: 0 })
+      if (!user) reject(createError.Unauthorized('Login to your account'))
+
+      resolve(payload.mobile)
+    })
+  })
+}
