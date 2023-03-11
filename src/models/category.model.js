bd385a3c0a1459f16e1fd7fd610070d6dd637abb
Alireza Askarpour <askarpourdev@gmail.com>
2023-03-11 15:00:32 -0500

    feat: add category model

---
+++ src/models/category.model.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+const { model, Schema } = mongoose
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true },
+  value: { type: String, required: true },
+  subcategories: [
+    {
+      name: { type: String, required: true },
+      value: { type: String, required: true },
+      disabled: { type: Boolean, default: false },
+    },
+  ],
+})
+
+export default model('category', CategorySchema)
