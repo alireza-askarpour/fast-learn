c09c4458b16d6aca26a5472e14b9bece290dcfc3
Alireza Askarpour <askarpourdev@gmail.com>
2023-03-17 18:07:49 -0400

    feat: add video upload middelware

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -17,6 +17,17 @@ const thumbnailStorage = multer.diskStorage({
   },
 })
 
+const videoStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const directory = './uploads/videos'
+    fs.mkdirSync(directory, { recursive: true })
+    return cb(null, directory)
+  },
+  filename: (req, file, cb) => {
+    cb(null, nanoid(alphabetLowerCaseLetters, 16) + path.extname(file.originalname))
+  },
+})
+
 const fileFilter = (req, file, cb) => {
   const ext = path.extname(file.originalname)
   const mimetypes = ['.jpg', '.jpeg', '.png', '.webp', '.gif']
@@ -24,10 +35,26 @@ const fileFilter = (req, file, cb) => {
   return cb(createHttpError.BadRequest('The submitted image format is not correct'))
 }
 
+function videoFilter(req, file, cb) {
+  const ext = path.extname(file.originalname)
+  const mimetypes = ['.mp4', '.mpg', '.mov', '.avi', '.mkv']
+  if (mimetypes.includes(ext)) {
+    return cb(null, true)
+  }
+  return cb(createError.BadRequest('The format of the video sent is not correct.'))
+}
+
 const pictureMaxSize = 1 * 1000 * 1000 // 1MB
+const videoMaxSize = 300 * 1000 * 1000 //300MB
 
 export const uploadCourseThumbnail = multer({
   storage: thumbnailStorage,
   fileFilter,
   limits: { fileSize: pictureMaxSize },
+})
+
+export const uploadEpisodeVideo = multer({
+  storage: videoStorage,
+  videoFilter,
+  limits: { fileSize: videoMaxSize },
 })
