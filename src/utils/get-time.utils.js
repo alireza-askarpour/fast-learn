8d71fcc4371c0946fa2cf5d17e4813ec7fe3811d
Alireza Askarpour <askarpourdev@gmail.com>
2023-03-17 17:44:04 -0400

    feat: add get time of course utils

--- src/utils/get-time.utils.js
+++ src/utils/get-time.utils.js
@@ -1,17 +1,45 @@
 export const getTimeOfEpisode = seconds => {
   let total = Math.round(seconds) / 60
   let [minutes, percent] = String(total).split('.')
-  let second = Math.round((percent * 60) / 100).toString().substring(0, 2)
+  let second = Math.round((percent * 60) / 100)
+    .toString()
+    .substring(0, 2)
   let houre = 0
   if (minutes > 60) {
-      total = minutes / 60
-      let [h1, percent] = String(total).split('.');
-      houre = h1,
-      minutes = Math.round((percent * 60) / 100).toString().substring(0, 2)
+    total = minutes / 60
+    let [h1, percent] = String(total).split('.')
+    ;(houre = h1),
+      (minutes = Math.round((percent * 60) / 100)
+        .toString()
+        .substring(0, 2))
   }
   if (String(houre).length == 1) houre = `0${houre}`
   if (String(minutes).length == 1) minutes = `0${minutes}`
   if (String(second).length == 1) second = `0${second}`
 
   return `${houre}:${minutes}:${second}`
+}
+
+export const getTimeOfCourse = (chapters = []) => {
+  let time,
+    hour,
+    minute,
+    second = 0
+  for (const chapter of chapters) {
+    if (Array.isArray(chapter?.episodes)) {
+      for (const episode of chapter.episodes) {
+        if (episode?.time) time = episode.time.split(':') // [hour, min, second]
+        else time = '00:00:00'.split(':')
+        if (time.length == 3) {
+          second += Number(time[0]) * 3600 // convert hour to second
+          second += Number(time[1]) * 60 // convert minute to second
+          second += Number(time[2]) //sum second with seond
+        } else if (time.length == 2) {
+          //05:23
+          second += Number(time[0]) * 60 // convert minute to second
+          second += Number(time[1]) //sum second with seond
+        }
+      }
+    }
+  }
 }
