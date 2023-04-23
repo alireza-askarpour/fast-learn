c47fc7e1dbff4c822040d8b6d57c09c5f2282bd4
Alireza Askarpour <askarpourdev@gmail.com>
2023-04-23 09:52:15 -0400

    feat: add get bookmarked blogs query

--- src/graphql/queries/user-profile.resolver.js
+++ src/graphql/queries/user-profile.resolver.js
@@ -1,11 +1,25 @@
 import { GraphQLList } from 'graphql'
 
 import { BlogType } from '../typeDefs/blog.type.js'
+import { CourseType } from '../typeDefs/course.type.js'
+
+import BlogModel from '../../models/blog.models.js'
 import CourseModel from '../../models/course.models.js'
+
 import { verifyAccessTokenInGraphQL } from '../../middlewares/authorization .middleware.js'
 
-export const getBookmarkedCourses = {
+export const getBookmarkedBlog = {
   type: new GraphQLList(BlogType),
+  resolve: async (_, args, context) => {
+    const { req } = context
+    const user = await verifyAccessTokenInGraphQL(req)
+    const blogs = await BlogModel.find({ bookmarks: user._id }).populate([{ path: 'author' }])
+    return blogs
+  },
+}
+
+export const getBookmarkedCourse = {
+  type: new GraphQLList(CourseType),
   resolve: async (_, args, context) => {
     const { req } = context
     const user = await verifyAccessTokenInGraphQL(req)
